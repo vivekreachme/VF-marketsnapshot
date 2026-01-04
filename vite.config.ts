5e47e318b9fc82e3b1181f9abe4a94de97bd3682
Vivek Kakar <vivek.reachme@gmail.com>
2026-01-04 16:34:49 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,5 +5,12 @@ export default defineConfig({
   plugins: [react()],
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+  },
+  server: {
+    port: 3000
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
   }
 });
